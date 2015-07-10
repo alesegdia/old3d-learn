34f6d94e5c2084d035ba8951b5f672c6ce913a72
Alejandro Seguí <alesegdia@gmail.com>
2015-07-10 12:34:48 +0200

    working on test sector render; adding sample sector; adding player angle; adding key handling

--- src/test/allapp_test_sector_render.h
+++ src/test/allapp_test_sector_render.h
@@ -12,6 +12,7 @@
 
 typedef struct {
 	vec3 pos;
+	float angle = M_PI;
 } player;
 
 class TestSectorRender : public AllegroApp {
@@ -26,13 +27,56 @@ public:
 
 	virtual ~TestSectorRender() {}
 
-	void HandleKeyInput() {
+	void Ready() {
+		sector s;
+		s.vertices.add(vec2(70,20));
+		s.vertices.add(vec2(70,70));
+		s.vertices.add(vec2(20,70));
+		s.vertices.add(vec2(20,20));
+		s.floor = 0;
+		s.ceil = 20;
+	}
 
+	void HandleKeyInput() {
+		if(key[KEY_UP]) {
+			pl.pos.y -= sin(pl.angle);
+			pl.pos.x -= cos(pl.angle);
+		}
+
+		if(key[KEY_DOWN] ) {
+			pl.pos.y += sin(pl.angle);
+			pl.pos.x += cos(pl.angle);
+		}
+
+		if(key[KEY_LEFT]) {
+			pl.angle -= 0.05;
+		}
+
+		if(key[KEY_RIGHT] ) {
+			pl.angle += 0.05;
+		}
+
+		if(key[KEY_SLEFT]) {
+			pl.pos.y -= sin(pl.angle+M_PI/2);
+			pl.pos.x -= cos(pl.angle+M_PI/2);
+		}
+
+		if( key[KEY_SRIGHT] ) {
+			pl.pos.y += sin(pl.angle+M_PI/2);
+			pl.pos.x += cos(pl.angle+M_PI/2);
+		}
+	}
+
+	void Draw() {
+		for( sector s : sectors ) {
+
+		}
 	}
 
 private:
 
 	player pl;
+	std::vector<sector> sectors;
 
 };
 
