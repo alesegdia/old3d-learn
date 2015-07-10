372f1fb0513ab2dd2ac03f2f26a067d6538b29f3
Alejandro Seguí <alesegdia@gmail.com>
2015-07-10 03:31:05 +0200

    changin allegro app test line list output folder

--- compile.sh
+++ compile.sh
@@ -9,4 +9,4 @@ function compile {
 compile test_single_line
 compile test_line_list
 
-g++ -o allapp src/app/allegroapp.cpp  src/test/allapp_test_line_list_app.cpp -lallegro -lallegro_primitives -std=c++11
+g++ -o bin/apptest_line_list src/app/allegroapp.cpp  src/test/allapp_test_line_list_app.cpp -lallegro -lallegro_primitives -std=c++11
